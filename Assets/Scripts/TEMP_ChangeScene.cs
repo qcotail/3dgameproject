eabf7267369fe9ea8560011674fd4d35c4863d8c
qcotail <kurtlara@protonmail.com>
2025-11-16 13:16:52 -0800

    Added Scene Changing Through Button
    You can press Spacebar and it will change through avaliable scenes.
    currently hardcoded to switch between kurt_scene and kurt_scene_2.

--- Assets/Scripts/TEMP_ChangeScene.cs
+++ Assets/Scripts/TEMP_ChangeScene.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class TEMP_ChangeScene : MonoBehaviour{
 	public InputActionReference newInput;
+
+	private float SpacebarInput;
+	static private TextMeshProUGUI UIText;
+
 	void Start(){
+		UIText = GetComponent<TextMeshProUGUI>();
 	}
 
 	void Update(){
-		Debug.Log(newInput.action);
+		SpacebarInput = newInput.action.ReadValue<float>();
+
+		if (SpacebarInput != 0){
+			Debug.Log(SpacebarInput);
+			UIText.text = "Spacebar is being pressed.";
+		}
+		else {
+			UIText.text = "AIUEO";
+		}
 	}
 }
---
+++ Assets/Scripts/TEMP_TextManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TEMP_TextManager : MonoBehaviour{
+	public InputActionReference newInput;
+
+	private float SpacebarInput;
+	private string CurrentSceneName;
+	private string NextSceneName;
+	static private TextMeshProUGUI UIText;
+
+	void Start(){
+		UIText = GetComponent<TextMeshProUGUI>();
+		CurrentSceneName = "";
+		NextSceneName = "";
+
+		Debug.Log("Start");
+		Debug.Log(UIText.GetInstanceID());
+	}
+
+	void Update(){
+		CurrentSceneName = SceneManager.GetActiveScene().name;
+		NextSceneName = "";
+		if (CurrentSceneName == "kurt_scene"){
+			NextSceneName = "kurt_scene_2";
+		}
+		if (CurrentSceneName == "kurt_scene_2") {
+			NextSceneName = "kurt_scene";
+		}
+
+		SpacebarInput = newInput.action.ReadValue<float>();
+		UIText.text = "Press Spacebar To Change Scene";
+		if (SpacebarInput != 0){
+			UIText.text = "Spacebar Pressed!";
+			SceneManager.LoadScene(NextSceneName);
+		}
+	}
+
+	void FixedUpdate(){
+		//Debug.Log("Cur:" + CurrentSceneName);
+		//Debug.Log("Next:"+NextSceneName);
+	}
+}
