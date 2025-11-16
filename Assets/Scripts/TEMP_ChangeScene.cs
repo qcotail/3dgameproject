660389344099e3aa54a65f4d8c3dd9ee3aa43c1e
qcotail <kurtlara@protonmail.com>
2025-11-16 09:37:50 -0800

    Messing with Unity Input System

--- Assets/Scripts/TEMP_ChangeScene.cs
+++ Assets/Scripts/TEMP_ChangeScene.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem
 
 public class TEMP_ChangeScene : MonoBehaviour{
+	public InputActionReference newInput;
 	void Start(){
 	}
+
 	void Update(){
+		Debug.Log(newInput.action);
 	}
 }
