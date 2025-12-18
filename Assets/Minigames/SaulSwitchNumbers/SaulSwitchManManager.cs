e0ac12ae19daa692a13787607dfc8fecc6636b69
MIGUEL <anthonymiguelochoa03@gmail.com>
2025-12-18 14:47:56 -0800

    Added Winning And Losing Video For SaulSwitchMan

--- Assets/Minigames/SaulSwitchNumbers/SaulSwitchManManager.cs
+++ Assets/Minigames/SaulSwitchNumbers/SaulSwitchManManager.cs
@@ -9,15 +9,34 @@ public class SaulSwitchManManager : MonoBehaviour
 
     [SerializeField] LevelTemplate lvltmp;
 
+    public GameObject saullossvideo;
+    public GameObject saulwinvideo;
+
+    public Canvas canvas;
+
     bool didWin;
     bool didFinish = false;
 
+    private void Start()
+    {
+        saullossvideo.SetActive(false);
+        saulwinvideo.SetActive(false);
+    }
     void Update()
     {
+        if (lvltmp.timer <= 0 && didFinish == false)
+        {
+            lvltmp.FinishMinigame(false);
+            canvas.enabled = false;
+            saullossvideo.SetActive(true);
+            didFinish = true;
+        }
         if (LeftCard == true && RightCard == true && didFinish == false)
         {
             didWin = true;
             lvltmp.FinishMinigame(didWin);
+            canvas.enabled = false;
+            saulwinvideo.SetActive(true);
             didFinish = true;
         }
     }
