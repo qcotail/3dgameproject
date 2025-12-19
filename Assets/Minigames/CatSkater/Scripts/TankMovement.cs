4ffa8f8d06df0a7826fc92418c5f4d8a59a46fd2
qcotail <kurtlara@protonmail.com>
2025-12-18 17:21:22 -0800

    cATsKaTER COMPLETED

--- Assets/Minigames/CatSkater/Scripts/TankMovement.cs
+++ Assets/Minigames/CatSkater/Scripts/TankMovement.cs
@@ -5,38 +5,49 @@ using UnityEngine;
 
 public class TankMovement : MonoBehaviour{
 	float x, z;                   /* PlayerInput        */
-	float speed = 12f;           /* F/B Speed          */
-	float turnSpeed = 0.8f;     /* L/R Speed          */
+	float speed = 20f;           /* F/B Speed          */
+	float turnSpeed = 50f;     /* L/R Speed          */
 	Rigidbody playerRigidBody; /* Player             */
+
+	private float startY;
+
+	[SerializeField] LevelTemplate lvltmp;
 
 	void Start(){
 		/* Assign Player to playerRigidBody */
 		playerRigidBody = GetComponent<Rigidbody>();
+		startY = playerRigidBody.position.y;
 	}
 
 	void Update(){
 		/* Get input values */
-		x = Input.GetAxis("Horizontal"); /* F/B */
-		z = Input.GetAxis("Vertical");  /* L/R */
-
-		/* Going backwards will be slower */
-
-		if (z != 0){
-			z /= 1.8f;
+		if (lvltmp.CanPlay()){
+			x = Input.GetAxis("Horizontal"); /* L/R */
+			z = Input.GetAxis("Vertical");  /* F/B */
 		}
 	}
 
 	private void FixedUpdate(){
 		/* Tank Controls; Left/Right Rotate */
-		Vector3 torque = Vector3.up * x * turnSpeed;
-		playerRigidBody.AddTorque(torque);
+		float turn = x * turnSpeed * Time.deltaTime;
+		Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+		playerRigidBody.MoveRotation(playerRigidBody.rotation * turnRotation);
 
 		/* Tank Controls; Forward/Back is Relative to Angle */
 		Vector3 velocity = transform.forward * z * speed;
 		playerRigidBody.velocity = velocity;
+
+		float curY = playerRigidBody.position.y;
+		if (curY != startY){
+			lvltmp.FinishMinigame(false);
+			playerRigidBody.useGravity = true;
+		}
 	}
 	void OnCollisionEnter(Collision c){
-		/* If You Bump You Game Over */
-		Debug.Log("Bump");
+		string cName = c.gameObject.name;
+
+		if (cName == "Success"){
+			lvltmp.FinishMinigame(true);
+		}
 	}
 }
